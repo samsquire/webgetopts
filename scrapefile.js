48164ae2114a638fb268acdeeec0088db6c3fb80
Sam Squire <ssquire@thoughtworks.com>
2012-12-02 12:59:51 +0000

    Simple scrape function used to extract metadata.

---
+++ scrapefile.js
@@ -0,0 +1,11 @@
+/*
+$ is your scrape function that should be bound. You could wrap up jQuery's $ or some other way of 'searching a file'.
+*/
+module.exports = function($, val, scrape) {
+	var scraped = {};
+	for (var target in scrape) {
+		var search = $(scrape[target]);
+			scraped[target] = val(search);
+	}	
+	return scraped;
+}
