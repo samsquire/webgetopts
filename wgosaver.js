c67377d069380ac6b6a806a622a24c4c6f6ad803
Sam Squire <ssquire@thoughtworks.com>
2012-12-02 14:43:02 +0000

    Now supports overrides incase the default hostname of the website is unacceptable.

--- wgosaver.js
+++ wgosaver.js
@@ -1,31 +1,36 @@
 module.exports = {
 
-fetch: function(address, callback){
-         var fs = require("fs"),
-         request = require("request"),
-         cheerio = require("cheerio"),
-         url = require('url'),
-         scrapefile = require("./scrapefile");
+fetch: function(address, overrides, callback){
+   var fs = require("fs"),
+       request = require("request"),
+       url = require('url'),
+       scrapefile = require("./jsonscrape.js"),
+       extraction = require('./metadata.json');
 
          request(address, function(error, response, body) {
-             var libxmljs = require("libxmljs");
-             console.log("Body is:", body);
-             var doc = libxmljs.parseHtmlString(body);
-             var $ = function(search) {
-               return doc.get(search);
-             }
+           var libxmljs = require("libxmljs");
+           var doc = libxmljs.parseHtmlString(body);
+           var $ = function(search) {
+             return doc.get(search);
+           }
+           var val = function(result) {
+             return result.value();
+           }
+           console.log(extraction);
+           var metadata = scrapefile($, val, extraction) 
 
-             var val = function(result) {
-               return result.value();
-             }
-            var extraction = require('./metadata.json');
-            console.log(extraction);
-            var metadata = scrapefile($, val, extraction) 
 
-             var URL = url.parse(response.request.uri.href);
-             metadata.target = url.resolve(URL.protocol + '//' + URL.host + URL.pathname, metadata.target);
-             metadata.site = URL.hostname; 
-         callback(metadata);
+           var URL = url.parse(response.request.uri.href);
+           metadata.target = url.resolve(URL.protocol + '//' + URL.host + URL.pathname, metadata.target);
+           metadata.site = URL.hostname; 
+
+           for (key in metadata) {
+            console.log(key);
+            if (key in overrides) {
+              metadata[key] = overrides[key];
+            }
+           } 
+           callback(metadata);
          });
 } 
 }
